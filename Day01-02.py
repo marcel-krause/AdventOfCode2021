5e679116feb214bfbd5b2b0e91d311c1abceaa64
Marcel Krause <32864708+marcel-krause@users.noreply.github.com>
2021-12-02 22:23:17 +0100

    Initial commit: add solutions for days 1 and 2

---
+++ Day01-02.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#Filename: Day01-02.py
+
+ ###############################################################################################################
+#                                                                                                               #
+#                               Advent of Code 2021 - Solutions of Problems                                     #
+#                                                                                                               #
+#   Author:     Marcel Krause                                                                                   #
+#   Date:       01.12.2021                                                                                      #
+#   Copyright:  Copyright (C) 2021, Marcel Krause                                                               #
+#   License:    GNU General Public License (GNU GPL-3.0-or-later)                                               #
+#                                                                                                               #
+#               This program is released under GNU General Public License (GNU GPL-3.0-or-later).               #
+#               This program is free software: you can redistribute it and/or modify it under the terms of the  #
+#               GNU General Public License as published by the Free Software Foundation, either version 3 of    #
+#               the License, or any later version.                                                              #
+#                                                                                                               #
+#               This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;       #
+#               without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.       #
+#               See the GNU General Public License for more details.                                            #
+#                                                                                                               #
+#               You have received a copy LICENSE.md of the GNU General Public License along with this program.  #
+#                                                                                                               #
+ ###############################################################################################################
+
+
+#------------------------------#
+#         Import Modules       #
+#------------------------------#
+import sys
+
+
+#----------------------------#
+#        Main Program        #
+#----------------------------#
+
+if __name__ == "__main__":
+    # Print the welcome screen
+    print('''
++------------------------------------+
+|                                    |
+|           Advent of Code           |
+|         Day 01 - Riddle 02         |
+|                                    |
++------------------------------------+
+    ''')
+
+    # Read the input file
+    with open('Day01-01_input.dat', 'r') as content_file:
+        all_lines = [int(line.rstrip('\n')) for line in content_file]
+
+    # Count the number of increases
+    num_increases = 0
+    for i in range(1, len(all_lines)-2):
+        if sum(all_lines[i:i+3]) > sum(all_lines[i-1:i+2]):
+            num_increases += 1
+
+    # Print the result
+    print("{} sums of three-measurement sliding windows are larger than the previous ones.".format(num_increases))
---
+++ Day02-01.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#Filename: Day02-01.py
+
+ ###############################################################################################################
+#                                                                                                               #
+#                               Advent of Code 2021 - Solutions of Problems                                     #
+#                                                                                                               #
+#   Author:     Marcel Krause                                                                                   #
+#   Date:       02.12.2021                                                                                      #
+#   Copyright:  Copyright (C) 2021, Marcel Krause                                                               #
+#   License:    GNU General Public License (GNU GPL-3.0-or-later)                                               #
+#                                                                                                               #
+#               This program is released under GNU General Public License (GNU GPL-3.0-or-later).               #
+#               This program is free software: you can redistribute it and/or modify it under the terms of the  #
+#               GNU General Public License as published by the Free Software Foundation, either version 3 of    #
+#               the License, or any later version.                                                              #
+#                                                                                                               #
+#               This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;       #
+#               without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.       #
+#               See the GNU General Public License for more details.                                            #
+#                                                                                                               #
+#               You have received a copy LICENSE.md of the GNU General Public License along with this program.  #
+#                                                                                                               #
+ ###############################################################################################################
+
+
+#----------------------------#
+#        Main Program        #
+#----------------------------#
+
+if __name__ == "__main__":
+    # Print the welcome screen
+    print('''
++------------------------------------+
+|                                    |
+|           Advent of Code           |
+|         Day 02 - Riddle 01         |
+|                                    |
++------------------------------------+
+    ''')
+
+    # Read the input file
+    with open('Day02-01_input.dat', 'r') as content_file:
+        all_lines = [line.rstrip('\n') for line in content_file]
+
+    # Find the final positions of the submarine
+    horizontal_position = 0
+    depth = 0
+    for line in all_lines:
+        curr_pos_change = int(line.split()[1])
+        
+        if 'forward' in line:
+            horizontal_position += curr_pos_change
+        if 'down' in line:
+            depth += curr_pos_change
+        if 'up' in line:
+            depth -= curr_pos_change
+
+    # Print the result
+    print("The product of the final horizontal position and depth is given by {}.".format(horizontal_position*depth))
